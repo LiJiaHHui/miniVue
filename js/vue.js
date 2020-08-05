bfc5991a3adfdbfdbedf0e4250bee3d3471dc3f3
e <1151329119@qq.com>
2020-08-05 20:43:29 +0800

    vue

---
+++ js/vue.js
@@ -0,0 +1,31 @@
+class Vue {
+    constructor (options) {
+    // 利用属性保存数据
+        this.$options = options || {}
+        this.$data = options.data || {}
+        // 传入字符串则为选择器，或是一个dom对象
+        this.$el = typeof options.el ==='string' ? document.querySelector(options.el) : options.el
+    // data中的成员转换为getter和setter，注入到vue实例，this指向vue实例
+        this._proxyData(this.$data)
+    }
+    // 代理data中的属性
+    _proxyData (data) {
+        // 获取data中的所有属性，返回一个数组
+        Object.keys(data).forEach(key => {
+            Object.defineProperty(this,key,{
+                enumerable: true,
+                configurable: true,
+                get () {
+                    return data[key]
+                },
+                set () {
+                    // 判断值是否相同，若相同直接返回
+                    if(newValue === data[key]){
+                        return
+                    }
+                    data[key] = newValue
+                }
+            })
+        })
+    }
+}
