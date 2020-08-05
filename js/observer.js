4ced355ef4a9a79a861918489c3e72dadacde6d4
e <1151329119@qq.com>
2020-08-05 23:33:56 +0800

    提交observer

---
+++ js/observer.js
@@ -0,0 +1,37 @@
+class Observer {
+    // 类创建完对象后，将属性马上转换为getter和setter，所以在构造函数中调用walk
+    constructor(data){
+        this.walk(data)
+    }
+    // 遍历对象的所有属性
+    walk (data) {
+        // 判断是否为对象
+        if(!data || typeof data!=='object'){
+            return
+        }
+        // 遍历data对象的所有属性
+        Object.keys(data).forEach(key=>{
+            this.defineReactive(data,key,data[key])
+        })
+    }
+    defineReactive (obj,key,val) {
+        let that=this
+        // 如果val是对象，把内部的属性转换为响应式
+        this.walk(val)
+        Object.defineProperty(obj,key,{
+            enumerable:true,
+            configurable:true,
+            get(){
+                return val
+            },
+            set(newValue){
+                if(newValue ===val){
+                    return
+                }
+                val=newValue
+                that.walk(newValue)
+            }
+        })
+
+    }
+}
--- js/vue.js
+++ js/vue.js
@@ -1,12 +1,14 @@
 class Vue {
     constructor (options) {
-    // 利用属性保存数据
+        // 利用属性保存数据
         this.$options = options || {}
         this.$data = options.data || {}
         // 传入字符串则为选择器，或是一个dom对象
         this.$el = typeof options.el ==='string' ? document.querySelector(options.el) : options.el
-    // data中的成员转换为getter和setter，注入到vue实例，this指向vue实例
+        // data中的成员转换为getter和setter，注入到vue实例，this指向vue实例
         this._proxyData(this.$data)
+        // 监听数据变化
+        new Observer(this.$data)
     }
     // 代理data中的属性
     _proxyData (data) {
@@ -18,7 +20,7 @@ class Vue {
                 get () {
                     return data[key]
                 },
-                set () {
+                set (newValue) {
                     // 判断值是否相同，若相同直接返回
                     if(newValue === data[key]){
                         return
